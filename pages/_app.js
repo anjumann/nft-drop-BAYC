b1401a62a7f1893f00c0943055766f00715384d5
anjuman raj <ayusmann23@gmail.com>
2022-08-27 08:58:07 +0530

    finished

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+    return (
+        <ThirdwebProvider
+            desiredChainId={ChainId.Rinkeby}
+            chainRpc={{
+                [ChainId.Rinkeby]: 'https://rinkeby.infura.io/v3/d8b05f3741db48dc97fd8d66f58646b4',
+            }}
+        >
+            <Component {...pageProps} />
+        </ThirdwebProvider>
+    )
 }
 
 export default MyApp
+
